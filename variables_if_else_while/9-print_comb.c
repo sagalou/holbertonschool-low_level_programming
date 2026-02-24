103a9ee3b57bb67505c0ba497608dc2b585e0135
sagalou <sagal.louise.haider@gmail.com>
2026-02-24 14:08:56 +0100

    Ajout du fichier 9-print_comb.c pour imprimer les chiffres de 0 à 9 séparés par des virgules

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lowercase followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+
+		i++;
+	}
+	return (0);
+}
