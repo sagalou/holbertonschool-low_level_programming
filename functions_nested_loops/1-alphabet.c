9d91ede006c07d2dc7c393fdde32212e7f3fbec9
sagalou <sagal.louise.haider@gmail.com>
2026-02-24 16:53:53 +0100

    Corrige la documentation de la fonction print_alphabet pour décrire correctement son comportement

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
-* main - Prints _putchar, followed by a new line
+* print_alphabet - prints the alphabet in lowercase
 *
-* Return: Always 0 (Success)
+* Return: void
 */
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 	_putchar(i);
