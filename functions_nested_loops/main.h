32b4f84c15e5681f60f4395b1b46e30b37c190b3
sagalou <sagal.louise.haider@gmail.com>
2026-02-24 15:32:19 +0100

    add a main.h for the task C-Funtions, nested loops

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,15 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h> /* Si tu as besoin de bibliothèques standards */
-
-/* Prototype de _putchar (fourni par Holberton) */
 int _putchar(char c);
 
-/* Tes prototypes de fonctions pour le projet */
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-/* ... ajoute les autres ici au fur et à mesure ... */
-
-#endif /* MAIN_H */
+#endif
