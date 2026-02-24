349ad5b9700ffad71ba929958186f6f06efdaba1
sagalou <sagal.louise.haider@gmail.com>
2026-02-24 11:34:38 +0100

    Ajout du fichier 7-print_tebahpla.c pour imprimer l'alphabet en minuscules à l'envers

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Prints the lozercase alphabet in reverse,
+* followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+	putchar('\n');
+	}
+	return (0);
+}
