4b452f5f94816812203e2f352447c65311778f6d
sagalou <sagal.louise.haider@gmail.com>
2026-02-24 20:22:15 +0100

    Ajoute la fonction _islower pour vérifier si un caractère est en minuscule

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,19 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - checks if a character is lowercase
- * @c: the character to check
- *
- * Return: 1 if lowercase, 0 otherwise
- */
+* _islower - checks if a character is lowercase
+* @c: the character to check
+*
+* Return: 1 if lowercase, 0 otherwise
+*/
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	{
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
