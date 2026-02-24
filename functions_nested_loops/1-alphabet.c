2df48a7b829f050741579eb9b6a757cff0384f83
sagalou <sagal.louise.haider@gmail.com>
2026-02-24 16:40:19 +0100

    Add new line to the main.h and 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - Prints _putchar, followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
