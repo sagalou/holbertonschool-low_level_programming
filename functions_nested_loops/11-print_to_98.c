9cf195806d78bcab35bfe9148f64e0fba916971b
sagalou <sagal.louise.haider@gmail.com>
2026-02-25 11:13:54 +0100

    Corrige la fonction print_to_98 pour améliorer la gestion des cas où n est supérieur à 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -16,11 +16,19 @@ void print_to_98(int n)
 }
 else if (n < 98)
 {
-
 while (n < 98)
 {
 printf("%d, ", n);
 n++;
+}
+printf("%d\n", 98);
+}
+else if (n > 98)
+{
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
 }
 printf("%d\n", 98);
 }
