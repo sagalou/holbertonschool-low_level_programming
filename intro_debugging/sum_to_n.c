0dc5816219432d208b0c2ed5ec447044a42b585b
sagalou <sagal.louise.haider@gmail.com>
2026-02-25 13:36:09 +0100

    Améliore la lisibilité du code dans sum_to_n.c en ajustant l'indentation et en ajoutant des espaces

--- intro_debugging/sum_to_n.c
+++ intro_debugging/sum_to_n.c
@@ -2,19 +2,19 @@
 
 int sum_to_n(int n)
 {
-    int i;
-    int sum = 0;
+	int i;
+	int sum = 0;
 
-    for (i = 1; i <= n; i++)
-        sum += i;
+	for (i = 1; i <= n; i++)
+		sum += i;
 
-    return (sum);
+	return (sum);
 }
 
 /* -- DO NOT Modify the code below this line -- */
 
 int main(void)
 {
-    printf("%d\n", sum_to_n(10));
-    return 0;
+	printf("%d\n", sum_to_n(10));
+	return 0;
 }
