81659cef38b2716617da6ac0e32179b823638cde
sagalou <sagal.louise.haider@gmail.com>
2026-02-26 13:02:59 +0100

    Ajoute la fonction print_most_numbers pour imprimer les chiffres de 0 à 9, en omettant 2 et 4.

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_numbers - Prints the numbers from 0 to 9, followed by a new line.
+*
+* Description: You can only use _putchar twice in your code.
+* This requires using a loop to iterate through the digits.
+*/
+void print_most_numbers(void)
+{
+    int i;
+
+    for (i = '0'; i <= '9'; i++)
+    {
+        if (i != '2' && i != '4')
+        {
+            _putchar(i);
+        }
+    }
+	_putchar('\n');
+}
