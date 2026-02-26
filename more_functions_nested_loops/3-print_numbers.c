76a443d02585aca42a16ceba8e859502230c2de3
sagalou <sagal.louise.haider@gmail.com>
2026-02-26 12:27:45 +0100

    Ajoute la fonction print_numbers pour imprimer les chiffres de 0 à 9 et la fonction _putchar pour écrire un caractère sur stdout.

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_numbers - Prints the numbers from 0 to 9, followed by a new line.
+*
+* Description: You can only use _putchar twice in your code.
+* This requires using a loop to iterate through the digits.
+*/
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	_putchar(i);
+}
+	_putchar('\n');
+}
