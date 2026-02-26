a1cbbb226a8e706bd2caa33dd1dc1669b69f2e39
sagalou <sagal.louise.haider@gmail.com>
2026-02-26 11:58:12 +0100

    Ajoute la fonction mul pour multiplier deux entiers et crée un fichier main pour tester cette fonction.

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* mul - Multiplies two integers.
+* @a: The first integer to multiply.
+* @b: The second integer to multiply.
+*
+* Return: The result of the multiplication (a * b).
+*/
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
