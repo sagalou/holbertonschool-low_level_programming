90777bae918662a4bc2e4cd4782b199f87357302
sagalou <sagal.louise.haider@gmail.com>
2026-03-03 10:56:16 +0100

    Add implementation of _puts and _putchar functions

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
- * void _puts
+ * _puts - point the length of a string
+ * @str: pointer to the string
  *
- * 
  */
 void _puts(char *str)
 {
-
+	while (*str != 0)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
