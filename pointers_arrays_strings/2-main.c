2f7b006311b5408fa2394b1ee0963c77949796fe
sagalou <sagal.louise.haider@gmail.com>
2026-03-04 09:13:01 +0100

    Remove unused main function and related code from 2-main.c

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -8,25 +8,6 @@
  */
 int main(void)
 {
-    char *str;
-    int len;
-
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
-}
-
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
     char s1[98];
     char *ptr;
     int i;
@@ -58,4 +39,3 @@ int main(void)
     printf("\n");
     return (0);
 }
-
