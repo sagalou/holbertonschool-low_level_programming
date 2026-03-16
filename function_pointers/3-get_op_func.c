522483c8df1054bfecfaee650afe028dd5436241
sagalou <sagal.louise.haider@gmail.com>
2026-03-16 17:30:31 +0100

    Add 3-calc task

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <string.h>
+
+/**
+ * get_op_func - selects the correct function for the operator
+ * @s: the operator as a string
+ *
+ * Return: pointer to the corresponding function, or NULL.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
