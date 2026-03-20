436de9a51c14ccd5c87a328a3c84c1c8909ebd48
sagalou <sagal.louise.haider@gmail.com>
2026-03-20 10:46:18 +0100

    Add 0-puts_recursion.c task

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - Prints a string
+ * @s: the string to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+
+    if (*s == '\0')
+    {
+        printf("\n");
+        return;
+    }
+    printf("%c", *s);
+    _puts_recursion(s + 1);
+}
+    
+   
