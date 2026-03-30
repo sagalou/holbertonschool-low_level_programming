a93c56774595ca6ed0ae10c60a8ff8f279e52d04
sagalou <sagal.louise.haider@gmail.com>
2026-03-30 17:43:57 +0200

    Add 6-hash_table_delete.c task

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	hash_node_t *next;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			next = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
