2796a661ebfa4c9e308ef01b4280f381c8d7f168
sagalou <sagal.louise.haider@gmail.com>
2026-04-08 15:57:02 +0200

    fix 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -24,46 +24,46 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_to == -1)
-    {
-        dprintf(2, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-    }
+	if (fd_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 
-    while ((r = read(fd_from, buf, 1024)) > 0)
-    {
-        w = write(fd_to, buf, r);
-        if (w == -1 || w != r)
-        {
-        dprintf(2, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-        }
-    }
+	while ((r = read(fd_from, buf, 1024)) > 0)
+	{
+		w = write(fd_to, buf, r);
+		if (w == -1 || w != r)
+		{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+		}
+	}
 
-    if (r == -1)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-    if (close(fd_from) == -1)
-    {
-        dprintf(2, "Error: Can't close fd %d\n", fd_from);
-        exit(100);
-    }
+	if (r == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
 
-    if (close(fd_to) == -1)
-    {
-        dprintf(2, "Error: Can't close fd %d\n", fd_to);
-        exit(100);
-    }
+	if (close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 
 return (0);
 
