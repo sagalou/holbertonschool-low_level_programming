82afff1f433ce87e4c18f924819427fd8ca58969
sagalou <sagal.louise.haider@gmail.com>
2026-04-08 15:53:45 +0200

    add 3-cp.c task

---
+++ file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - copies content from one file to another
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from;    /* file descriptor fichier source */
+	int fd_to;      /* file descriptor fichier destination */
+	ssize_t r;      /* octets lus par read */
+	ssize_t w;      /* octets écrits par write */
+	char buf[1024]; /* le buffer de 1024 octets */
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1)
+    {
+        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+
+   fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (fd_to == -1)
+    {
+        dprintf(2, "Error: Can't write to %s\n", argv[2]);
+        exit(99);
+    }
+
+    while ((r = read(fd_from, buf, 1024)) > 0)
+    {
+        w = write(fd_to, buf, r);
+        if (w == -1 || w != r)
+        {
+        dprintf(2, "Error: Can't write to %s\n", argv[2]);
+        exit(99);
+        }
+    }
+
+    if (r == -1)
+    {
+        dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+    if (close(fd_from) == -1)
+    {
+        dprintf(2, "Error: Can't close fd %d\n", fd_from);
+        exit(100);
+    }
+
+    if (close(fd_to) == -1)
+    {
+        dprintf(2, "Error: Can't close fd %d\n", fd_to);
+        exit(100);
+    }
+
+return (0);
+
+}
