0d176cde275d513fa6bab98233aea8d77689400c
sagalou <sagal.louise.haider@gmail.com>
2026-04-08 14:09:36 +0200

    feat main h add 1-create_file file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * create_file - Creates a file and writes text content to it
+ * @filename: Name of the file to create
+ * @text_content: String to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+
+	int fd;
+	int w;
+
+	if (filename == NULL)
+		return(-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	w = write(fd, text_content, strlen(text_content));
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (w);
+
+}
