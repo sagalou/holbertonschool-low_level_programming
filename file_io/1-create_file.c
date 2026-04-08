b32352d3c929cbaa087f1067df13eb148465f9eb
sagalou <sagal.louise.haider@gmail.com>
2026-04-08 15:13:10 +0200

    fix 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,6 +22,12 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
 	w = write(fd, text_content, strlen(text_content));
 	if (w == -1)
 	{
